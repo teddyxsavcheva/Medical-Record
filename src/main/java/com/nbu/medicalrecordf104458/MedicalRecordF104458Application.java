15aa403f3770ed84abdc1f7294baf91664e806b3
Teodora Savcheva <teddyxsavcheva02@gmail.com>
2025-01-09 19:00:14 +0200

    Initial commit for correct project

---
+++ src/main/java/com/nbu/medicalrecordf104458/MedicalRecordF104458Application.java
@@ -0,0 +1,13 @@
+package com.nbu.medicalrecordf104458;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalRecordF104458Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalRecordF104458Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/nbu/medicalrecordf104458/MedicalRecordF104458ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbu.medicalrecordf104458;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalRecordF104458ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
